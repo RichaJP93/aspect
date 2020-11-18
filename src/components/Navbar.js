063bd831f15b5e06a83469ef231e3f2bf04d7aac
Joseph Richardson <richardsonjp93@gmail.com>
2020-11-18 12:38:38 +0000

    Basic layout for home +navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,11 +2,26 @@ import React from 'react'
 
 function Navbar(){
   return (
-    <nav className="navbar navbar-dark">
-      <a className="navbar-brand" href='/'>ASPECT</a>
+    <nav className="navbar">      
+      <a className="navbar-item" href='/'>ASPECT</a>      
+      <ul className='navbar-links'>        
+        <li>
+          <a className="navbar-item" href='/about'>About</a>
+        </li>
+      </ul>
+      
     </nav>
   )
 }
 
 export default Navbar;
 
+{/* <li className="nav-item">
+							<Link
+								to="/products"
+								className="nav-links"
+								onClick={closeMobileMenu}
+							>
+								Products
+							</Link>
+						</li> */}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,9 @@
 function Home () {
   return (
-    <>     
+    <>
+      <h1>Welcome to ASPECT!</h1> 
+      <h3>Create a new room</h3>
+      <h3>Have a room code already? Join here!</h3>
     </>
   )
 }
