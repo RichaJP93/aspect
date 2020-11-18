5d2181dbab4d0050607236d716da792d61eda824
Joseph Richardson <richardsonjp93@gmail.com>
2020-11-18 11:58:14 +0000

    set up initial react app + navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Navbar(){
+  return (
+    <nav className="navbar navbar-dark">
+      <a className="navbar-brand" href='/'>ASPECT</a>
+    </nav>
+  )
+}
+
+export default Navbar;
+
