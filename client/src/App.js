893cbf5f7b9cd1e6d213d09a2bc2330aa8734ecf
Joseph Richardson <richardsonjp93@gmail.com>
2021-02-25 13:01:21 +0000

    added api folder + started models for db

--- client/src/App.js
+++ client/src/App.js
@@ -4,21 +4,45 @@ import Home from './pages/Home.js';
 import SetupPage from './pages/SetupPage.js'
 import GameRoom from './pages/GameRoom.js'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Component } from 'react';
 
 
-function App() {
-  return (
-    <>
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route path="/" exact component={Home}/>  
-          <Route path="/setup" component={SetupPage}/>
-          <Route path="/lobby" component={GameRoom}/>
-        </Switch>
-      </Router>
-    </>
-  );
+class App extends Component {
+  state = {
+    data: null
+  };
+
+  componentDidMount() {
+    this.callAPI()
+      .then(res => console.log(res.express))
+      .catch(err => console.log(err));
+  }
+
+  callAPI = async () => {
+    const response = await fetch('/express_backend');
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message) 
+    }
+    return body;
+  }
+
+  render(){
+    return (
+      <>
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route path="/" exact component={Home}/>  
+            <Route path="/setup" component={SetupPage}/>
+            <Route path="/lobby" component={GameRoom}/>
+          </Switch>
+        </Router>
+      </>
+    );
+  }
+  
 }
 
 export default App;
